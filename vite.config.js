a2c4688652326a1f2cf4517be3e315d708498701
Drivello <Nico_dd@outlook.com.ar>
2025-05-24 20:23:30 -0300

    cd

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Cambia esto si vas a hospedar la app en una subruta diferente
+  base: '/3d-printer-cost-calculator/', // Cambia esto si vas a hospedar la app en una subruta diferente
 });
